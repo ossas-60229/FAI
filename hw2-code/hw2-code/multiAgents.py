f342f79784fb4763a331c62422526ae16780eb34
ossas-60229 <ossas02066@LAPTOP-72G60OQ9>
2023-04-06 16:54:00 +0800

    31246

--- hw2-code/hw2-code/multiAgents.py
+++ hw2-code/hw2-code/multiAgents.py
@@ -87,49 +87,56 @@ class ReflexAgent(Agent):
         GhostNum = len(newGhostPos)
         FoodNum = len(newFood.asList())
         ret_score = successorGameState.getScore()
-        min_to_g =  sys.maxsize
-        ind = 0
+        min_to_g = sys.maxsize
+        ind_g = 0
+        direct_g = 0
         for i in range(GhostNum):
             to_g = the_dist(newPos, newGhostPos[i])
             if to_g < min_to_g:
                 min_to_g = to_g
-                ind = i
+                ind_g = i
         min_to_f = sys.maxsize
         for i in range(FoodNum):
             to_f = the_dist(newPos, newFood.asList()[i])
             food_pos = newFood.asList()[i]
-            offset = 50
+            offset = 100
             times = 0
-            # to NORTH 
+            outa = 0
+            # to NORTH
             if food_pos[1] > newPos[1]:
+                outa += 1
                 if 'North' not in now_legal_actions:
-                    to_f += offset*times
                     times += 1
             # to SOUTH
             if food_pos[1] < newPos[1]:
+                outa += 1
                 if 'South' not in now_legal_actions:
-                    to_f += offset*times
                     times += 1
             # to EAST
             if food_pos[0] > newPos[0]:
+                outa += 1
                 if 'East' not in now_legal_actions:
-                    to_f += offset*times
                     times += 1
             # to WEST
             if food_pos[0] < newPos[0]:
+                outa += 1
                 if 'West' not in now_legal_actions:
-                    to_f += offset*times
                     times += 1
+            if outa == times:
+                to_f += offset*times
             if to_f < min_to_f:
                 min_to_f = to_f
         if action == 'Stop':
-            ret_score -= 30
-        ret_score += min_to_g / (min_to_f * 10 + 1)
-        if(FoodNum == 0):
-            ret_score = sys.maxsize
-        if min_to_g <= 1:
-            ret_score = -sys.maxsize
+            ret_score -= 50
+        KEIKAI = 2
+        if GhostNum == 0:
+            min_to_g = KEIKAI + 10
+        if min_to_g <= KEIKAI and newScaredTimes[ind_g] < KEIKAI*2:
+            ret_score -= 250/(min_to_g + 1)
+        else:
+            ret_score += 200 / (min_to_f*FoodNum*10 + 1)
         return ret_score
+
 
 def scoreEvaluationFunction(currentGameState: GameState):
     """
@@ -192,7 +199,54 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+        return self.Minimax_eval(gameState, 0, 0, -sys.maxsize)[1]
+    # evaluation function (MinMax ver.)
+
+    def Minimax_eval(self, gameState: GameState, index: int, depth: int, maxmin: float):
+        # the leaf state
+        legal_moves = gameState.getLegalActions(index)
+        ghost_num = gameState.getNumAgents() - 1
+        if depth == self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState), ""
+        else:
+            # This is PACMAN (max agent)
+
+            if index == 0:
+                max_val = -sys.maxsize
+                max_move = ""
+                for lmove in legal_moves:
+                    successor = gameState.generateSuccessor(index, lmove)
+                    new_index = index + 1
+                    new_depth = depth
+                    tmp_val = self.Minimax_eval(
+                        successor, new_index, new_depth, max_val)[0]
+                    if tmp_val > max_val:
+                        max_val = tmp_val
+                        max_move = lmove
+                return (max_val, max_move)
+            # This is GHOST (min agent)
+            elif index > 0:
+                min_val = sys.maxsize
+                min_move = ""
+                for lmove in legal_moves:
+                    successor = gameState.generateSuccessor(index, lmove)
+                    new_index = index + 1
+                    new_depth = depth
+                    if new_index > ghost_num:
+                        new_index = 0
+                        new_depth += 1
+                    tmp_val = self.Minimax_eval(
+                        successor, new_index, new_depth, maxmin)[0]
+                    # if tmp_val < maxmin:
+                    # maxmin = tmp_val
+                    # min_move = lmove
+                    # break
+                    if tmp_val < min_val:
+                        min_val = tmp_val
+                        min_move = lmove
+                return (min_val, min_move)
+        return (0, "")
 
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
@@ -205,7 +259,61 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+        return self.alphabeta_eval(gameState, 0, 0, -sys.maxsize, sys.maxsize)[1]
+    # evaluation function (AlphaBeta ver.)
+
+    def alphabeta_eval(self, gameState: GameState, index: int, depth: int, alpha: float, beta: float):
+        # the leaf state
+        if depth == self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState), ""
+        legal_moves = gameState.getLegalActions(index)
+        ghost_num = gameState.getNumAgents() - 1
+        # This is PACMAN (max agent)
+
+        if index == 0:
+            max_val = -sys.maxsize
+            tmp_alpha = alpha
+            max_move = ""
+            for lmove in legal_moves:
+                successor = gameState.generateSuccessor(index, lmove)
+                new_index = index + 1
+                new_depth = depth
+                tmp_alpha = max(tmp_alpha, max_val)
+                tmp_val = self.alphabeta_eval(
+                    successor, new_index, new_depth, tmp_alpha, beta)[0]
+                if tmp_val > max_val:
+                    max_val = tmp_val
+                    max_move = lmove
+                if max_val > beta:
+                    return (max_val, max_move)
+            return (max_val, max_move)
+        # This is GHOST (min agent)
+        elif index > 0:
+            min_val = sys.maxsize
+            min_move = ""
+            tmpbeta = beta
+            for lmove in legal_moves:
+                successor = gameState.generateSuccessor(index, lmove)
+                new_index = index + 1
+                new_depth = depth
+                if new_index > ghost_num:
+                    new_index = 0
+                    new_depth += 1
+                # if tmp_val < maxmin:
+                # maxmin = tmp_val
+                # min_move = lmove
+                # break
+                tmpbeta = min(tmpbeta, min_val)
+                tmp_val = self.alphabeta_eval(
+                    successor, new_index, new_depth, alpha, tmpbeta)[0]
+                if tmp_val < min_val:
+                    min_val = tmp_val
+                    min_move = lmove
+                if min_val < alpha:
+                    return (min_val, min_move)
+            return (min_val, min_move)
+        return (0, "")
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
@@ -221,18 +329,172 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+        return self.Expectimax_eval(gameState, 0, 0, 0)[1]
+
+    def Expectimax_eval(self, gameState: GameState, index: int, depth: int, maxmin: float):
+        # the leaf state
+        legal_moves = gameState.getLegalActions(index)
+        ghost_num = gameState.getNumAgents() - 1
+        if depth == self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState), ""
+        else:
+            # This is PACMAN (max agent)
+            if index == 0:
+                max_val = -sys.maxsize
+                max_move = ""
+                for lmove in legal_moves:
+                    successor = gameState.generateSuccessor(index, lmove)
+                    new_index = index + 1
+                    new_depth = depth
+                    tmp_val = self.Expectimax_eval(
+                        successor, new_index, new_depth, max_val)[0]
+                    if tmp_val > max_val:
+                        max_val = tmp_val
+                        max_move = lmove
+                return (max_val, max_move)
+            # This is GHOST (expect agent)
+            elif index > 0:
+                min_val = sys.maxsize
+                min_move = ""
+                exp_val = 0
+                for lmove in legal_moves:
+                    successor = gameState.generateSuccessor(index, lmove)
+                    new_index = index + 1
+                    new_depth = depth
+                    if new_index > ghost_num:
+                        new_index = 0
+                        new_depth += 1
+                    tmp_val = self.Expectimax_eval(
+                        successor, new_index, new_depth, maxmin)[0]
+                    exp_val += tmp_val
+                min_val = float(exp_val) / len(legal_moves)
+                min_move = ""
+                return (min_val, min_move)
+        return (0, "")
 
 
 def betterEvaluationFunction(currentGameState: GameState):
+    if currentGameState.isWin():
+        return scoreEvaluationFunction(currentGameState) + 10000
+    if currentGameState.isLose():
+        return -10000
+
+    def the_dist(pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
     """
     Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
     evaluation function (question 5).
 
     DESCRIPTION: <write something here so we know what you did>
+    desciption: 
+        1.calculate the lowest distance between pacman and ghost
+        2.if it is dangerous, applay alphabeta pruning
+        3.if it is safe, apply greedy method(eat food first)
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # util.raiseNotDefined()
+    Gnum = currentGameState.getNumAgents() - 1
+
+    def alphabeta_eval(gameState: GameState, index: int, depth: int, alpha: float, beta: float):
+        # the leaf state
+        if depth == 2 or gameState.isWin() or gameState.isLose():
+            return scoreEvaluationFunction(gameState), ""
+        legal_moves = gameState.getLegalActions(index)
+        ghost_num = gameState.getNumAgents() - 1
+        # This is PACMAN (max agent)
+
+        if index == 0:
+            max_val = -sys.maxsize
+            tmp_alpha = alpha
+            max_move = ""
+            for lmove in legal_moves:
+                successor = gameState.generateSuccessor(index, lmove)
+                new_index = index + 1
+                new_depth = depth
+                tmp_alpha = max(tmp_alpha, max_val)
+                tmp_val = alphabeta_eval(
+                    successor, new_index, new_depth, tmp_alpha, beta)[0]
+                if lmove == "stop":
+                    tmp_val = -sys.maxsize
+                if tmp_val > max_val:
+                    max_val = tmp_val
+                    max_move = lmove
+                if max_val > beta:
+                    return (max_val, max_move)
+            return (max_val, max_move)
+        # This is GHOST (min agent)
+        elif index > 0:
+            min_val = sys.maxsize
+            min_move = ""
+            tmpbeta = beta
+            for lmove in legal_moves:
+                successor = gameState.generateSuccessor(index, lmove)
+                new_index = index + 1
+                new_depth = depth
+                if new_index > ghost_num:
+                    new_index = 0
+                    new_depth += 1
+                # if tmp_val < maxmin:
+                # maxmin = tmp_val
+                # min_move = lmove
+                # break
+                tmpbeta = min(tmpbeta, min_val)
+                tmp_val = alphabeta_eval(
+                    successor, new_index, new_depth, alpha, tmpbeta)[0]
+                if tmp_val < min_val:
+                    min_val = tmp_val
+                    min_move = lmove
+                if min_val < alpha:
+                    return (min_val, min_move)
+            return (min_val, min_move)
+        return (0, "")
+    ghost_poss = currentGameState.getGhostPositions()
+    pacman_poss = currentGameState.getPacmanPosition()
+    ghost_dist = [(the_dist(ghost_poss[i], pacman_poss), i + 1)
+                  for i in range(Gnum)]
+    ghost_dist.sort()
+    food_pos = currentGameState.getFood().asList()
+    min_f_dist = sys.maxsize
+    min_food = food_pos[0]
+    legal_moves = currentGameState.getLegalActions(0)
+    for fpos in food_pos:
+        dist = the_dist(fpos, pacman_poss)
+        offset = 20
+        time = 0
+        ff = 0
+        if min_food[0] > pacman_poss[0]:
+            ff += 1
+            if "East" not in legal_moves:
+                time += 1
+        if min_food[0] < pacman_poss[0]:
+            ff += 1
+            if "West" not in legal_moves:
+                time += 1
+        if min_food[1] < pacman_poss[1]:
+            ff += 1
+            if "South" not in legal_moves:
+                time += 1
+        if min_food[1] > pacman_poss[1]:
+            ff += 1
+            if "North" not in legal_moves:
+                time += 1
+        if ff == time:
+            dist += offset*time
+        if dist < min_f_dist:
+            min_f_dist = dist
+            min_food = fpos
+    if ghost_dist[0][0] <= 3:
+        ffss = alphabeta_eval(
+            currentGameState, 0, 0, -sys.maxsize, sys.maxsize)
+        if (ffss[1] == "Stop"):
+            return -sys.maxsize
+        else:
+            return ffss[0]
+    else:
+        min_max_val = scoreEvaluationFunction(
+            currentGameState) + ghost_dist[0][0]/(min_f_dist*10 + 1)
+    return min_max_val
 
 
 # Abbreviation
